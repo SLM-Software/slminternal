20893298ad594f9f410e4f398bc5379635dd566c
Scott Yacko <scott@yackofamily.com>
2017-09-06 11:44:14 -0700

    Add DB container to access Postgres

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,14 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// database - postgres
+$container['db'] = function ($c)
+{
+	$settings = $c->get('settings')['db'];
+	$pdo = new PDO('pgsql:host=localhost;dbname=slmdb;port=5432', 'slmadmin', 'Yidiao01');
+	$pdo->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $pdo;
+};
