3ba60d939611afbf250a2aafb676d3f0eace7c34
Scott Yacko <scott@yackofamily.com>
2017-09-22 18:25:01 -0700

    Made Request and String classes for getCustomer
    Pulled common code from getCustomerMessage_Request and getCustomerMessage_String to getCustomerMessage method.

--- src/classes/SLMCustomerMessage.php
+++ src/classes/SLMCustomerMessage.php
@@ -14,6 +14,7 @@
 
 namespace API;
 
+use Slim;
 /**
  * Class SLMCustomerMessage
  */
@@ -44,9 +45,7 @@ class SLMCustomerMessage extends SLMInternal
 	 *
 	 * @api
 	 *
-	 * @param ServerRequestInterface $request The request object implements the PSR 7
-	 *                                        ServerRequestInterface with which you can inspect and
-	 *                                        manipulate the HTTP request method, headers, and body.
+	 * @param Slim/Http/Request $request
 	 *
 	 *          The query elements in the URI are as follow:
 	 *          Required elements:
@@ -65,12 +64,57 @@ class SLMCustomerMessage extends SLMInternal
 	 *                      custMsg is the message that is displayed to the end user (customer or member)
 	 *                      retPack is the payload that is return to the caller
 	 */
-	public function getCustomerMessage($request)
+	public function getCustomerMessage_Request(Slim\Http\Request $request)
 	{
 		$this->myLogger->debug(__METHOD__);
 
 		$myCMTag = strtolower($request->getQueryParam('cmtag'));
 		$myLocaleId = strtoupper($request->getQueryParam('localeid'));
+		return $this->getCustomerMessage($myCMTag, $myLocaleId);
+	}
+
+	/**
+	 * This will get a customer message from the database
+	 *
+	 * Based on the parameters supplied in the call, this method will return a message in the langauge requested.
+	 *
+	 * @api
+	 *
+	 * @param String $cmTag - This is the customer message tag
+	 * @param String $localeId - This is the locale language id used.
+	 *
+	 *          The query elements in the URI are as follow:
+	 *          Required elements:
+	 *              cmtag   = customer message tag [varchar(50)]
+	 *              localeid = local idenitifer [varchar(10)]
+	 *
+	 *          Option elements:
+	 *              none at this time.
+	 *
+	 * @todo What still needs to be done before going to production
+	 *
+	 * @return array  Keys: errCode, statusText, codeLoc, custMsg, retPack
+	 *                      errCode is 0 for Success or 900 for error
+	 *                      statusText contains system generated error message for debugging
+	 *                      codeLoc is the class and method that throw the error
+	 *                      custMsg is the message that is displayed to the end user (customer or member)
+	 *                      retPack is the payload that is return to the caller
+	 */
+	public function getCustomerMessage_String(string $cmTag, string $localeId)
+	{
+		$this->myLogger->debug(__METHOD__);
+
+		return $this->getCustomerMessage($cmTag, $localeId);
+	}
+
+	/**
+	 * @param string $myCMTag
+	 * @param string $myLocaleId
+	 *
+	 * @return array
+	 */
+	protected function getCustomerMessage(string $myCMTag, string $myLocaleId)
+	{
 		$qCustMessage = $this->myDB->prepare('select customermessage from slm.customermessages where lovkey = :cmtag and localeidentifier = :localeid');
 		$qCustMessage->bindParam(':cmtag', $myCMTag, $this->myDB::PARAM_STR);
 		$qCustMessage->bindParam(':localeid', $myLocaleId, $this->myDB::PARAM_STR);
@@ -78,8 +122,18 @@ class SLMCustomerMessage extends SLMInternal
 		try
 		{
 			$qCustMessage->execute();
+			$this->myLogger->debug('Row Count:' . $qCustMessage->rowCount());
 			$custMessage = $qCustMessage->fetch();
-			$resultString = array('errCode' => 0, 'statusText' => 'Success', 'codeLoc' => '', 'custMsg' => $custMessage, 'retPack' => '');
+			if ($qCustMessage->rowCount() == 0 )
+			{
+				$this->myLogger->debug('Row Count is ZERO');
+				$resultString = array('errCode' => 200, 'statusText' => 'Not Found', 'codeLoc' => '', 'custMsg' => $custMessage, 'retPack' => '');
+			}
+			else
+			{
+				$this->myLogger->debug('Row Count is greater than zero');
+				$resultString = array('errCode' => 0, 'statusText' => 'Success', 'codeLoc' => '', 'custMsg' => $custMessage, 'retPack' => '');
+			}
 		} catch (PDOException $e)
 		{
 			$myException = $e;
