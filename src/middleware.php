30909e9040f01b272f36bda3b55ff0ac9f20aed0
Scott Yacko <scott@yackofamily.com>
2018-03-22 09:23:34 -0700

    create skeleton middleware auth

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,36 @@
 <?php
 // Application middleware
-
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+use Auth0\SDK\JWTVerifier;
+use Auth0\SDK\Exception\CoreException;
+use Auth0\SDK\Exception\InvalidTokenException;
+
+class Middleware
+{
+	protected $container;
+
+	public function __construct($container)
+	{
+		$this->container = $container;
+		$this->container->logger->debug(__METHOD__);
+	}
+
+	/**
+	 * Short Description
+	 *
+	 * Long Description
+	 *
+	 */
+	public function __invoke($request, $response, $next)
+	{
+		$this->container->logger->debug(__METHOD__);
+
+		$x = $_SERVER[REMOTE_HOST];
+		$this->container->logger->debug("\$x=$x");
+
+		$myResponse = $next($request, $response);
+
+		return $myResponse;
+	}
+}
--- src/routes.php
+++ src/routes.php
@@ -4,43 +4,34 @@
 // For Testing only
 //var_dump($_SERVER);
 
-$app->get('/edeninternal/getdeviceid', function ($request, $response, $args)
-{
-	$this->logger->info("/getdeviceid '/' route");
-	$myEDENInternal = new \API\EDENInternal($this->logger);
+$app->group('', function(){
+	$this->get('/edeninternal/getdeviceid', function ($request, $response, $args)
+	{
+		$this->logger->info("/getdeviceid '/' route");
+		$myEDENInternal = new \API\EDENInternal($this->logger);
 
-	return $response->withJson($myEDENInternal->getDeviceId());
-});
+		return $response->withJson($myEDENInternal->getDeviceId());
+	});
+	$this->get('/edeninternal/validatedeviceid', function ($request, $response, $args)
+	{
+		$this->logger->info("/validatedeviceid '/' route");
+		$myEDENInternal = new \API\EDENInternal($this->logger);
 
-$app->get('/edeninternal/validatedeviceid', function ($request, $response, $args)
-{
-	$this->logger->info("/validatedeviceid '/' route");
-	$myEDENInternal = new \API\EDENInternal($this->logger);
+		return $response->withJson($myEDENInternal->validateDeviceIdFormat_Request($request));
+	});
+	$this->get('/edeninternal/version', function ($request, $response, $args)
+	{
+		$this->logger->info("version '/' route");
+		$curlSettings = $this->get('settings')['curl'];
+		$myEDENInternal = new \API\EDENInternal($this->logger, $curlSettings);
 
-	return $response->withJson($myEDENInternal->validateDeviceIdFormat_Request($request));
-});
+		return $response->withJson($myEDENInternal->getVersion());
+	});
+	$this->get('/edeninternal/getcustomermessage', function ($request, $response, $args)
+	{
+		$this->logger->info("getCustomerMessage '/' route");
+		$myEDENCustomerMessage = new \API\EDENCustomerMessage($this->logger, $this->db);
 
-$app->get('/edeninternal/version', function ($request, $response, $args)
-{
-	$this->logger->info("version '/' route");
-	$curlSettings = $this->get('settings')['curl'];
-	$myEDENInternal = new \API\EDENInternal($this->logger, $curlSettings);
-
-	return $response->withJson($myEDENInternal->getVersion());
-});
-
-$app->get('/edeninternal/getcustomermessage', function ($request, $response, $args)
-{
-	$this->logger->info("getCustomerMessage '/' route");
-	$myEDENCustomerMessage = new \API\EDENCustomerMessage($this->logger, $this->db);
-
-	return $response->withJson($myEDENCustomerMessage->getCustomerMessage_Request($request));
-});
-
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("EDENInternal:catch-all '/' route");
-	$data = array('firstName' => 'Scott', 'lastName' => 'Yacko');
-
-	return $response->withJson($data);
-});
+		return $response->withJson($myEDENCustomerMessage->getCustomerMessage_Request($request));
+	});
+})->add(new Middleware($container));
