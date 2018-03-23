31d2e15fc4d931be204a54f82e0470032249b800
Scott Yacko <scott@yackofamily.com>
2018-03-23 10:37:10 -0700

    Put Logger back - there was a coding dependency

--- tests/unit/EDENInternalTest.php
+++ tests/unit/EDENInternalTest.php
@@ -41,6 +41,11 @@ class EDENInternalTest extends \Codeception\Test\Unit
 // Register routes
 		require __DIR__ . '/../../src/routes.php';
 
+// Start Logger
+		$this->logger = new Monolog\Logger($this->settings['settings']['logger']['name']);
+		$this->logger->pushProcessor(new Monolog\Processor\UidProcessor());
+		$this->logger->pushHandler(new Monolog\Handler\StreamHandler($this->settings['settings']['logger']['path'], $this->settings['settings']['logger']['level']));
+
 		$app->run();
 	}
 
